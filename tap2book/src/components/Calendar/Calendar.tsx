ba046b5dfbda63753390fc8b8fb4590e5d6d727c
tetraminz <agero.flynn@gmail.com>
2025-01-05 22:31:38 +0800

    calendar style import

--- tap2book/src/components/Calendar/Calendar.tsx
+++ tap2book/src/components/Calendar/Calendar.tsx
@@ -12,9 +12,19 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
+import {themeParams, useSignal} from "@telegram-apps/sdk-react";
 
 export function DatePickerDemo() {
     const [date, setDate] = React.useState<Date>()
+    const theme = useSignal(themeParams.state)
+
+    // Динамические стили на основе темы Telegram
+    const styles = {
+        button: {
+            backgroundColor: theme.buttonColor,
+            color: theme.buttonTextColor,
+        } as React.CSSProperties
+    }
 
     return (
         <div>
@@ -22,6 +32,7 @@ export function DatePickerDemo() {
                 <PopoverTrigger asChild>
                     <Button
                         variant={"default"}
+                        style={styles.button}
                         className={cn(
                             "w-[280px] justify-start text-left font-normal",
                             !date && "text-muted-foreground"
@@ -31,12 +42,16 @@ export function DatePickerDemo() {
                         {date ? format(date, "PPP") : <span>Pick a date</span>}
                     </Button>
                 </PopoverTrigger>
-                <PopoverContent >
+                <PopoverContent>
                     <Calendar className="w-auto p-0"
                         mode="single"
                         selected={date}
                         onSelect={setDate}
                         initialFocus
+                        classNames={{
+                            day_selected: `bg-[var(--tg-theme-button-color)] text-[var(--tg-theme-button-text-color)]`,
+                            day_today: "bg-[var(--tg-theme-hint-color)] text-[var(--tg-theme-text-color)]",
+                        }}
                     />
                 </PopoverContent>
             </Popover>
