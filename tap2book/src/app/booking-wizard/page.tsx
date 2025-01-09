5dea9bb74903b5aadc6566c6d83385a93e61ab0b
tetraminz <agero.flynn@gmail.com>
2025-01-10 00:26:15 +0800

    fix wizard page.

--- tap2book/src/app/booking-wizard/page.tsx
+++ tap2book/src/app/booking-wizard/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import { Page } from "@/components/Page";
-import { Modal } from "@telegram-apps/telegram-ui";
-import { Button } from "@/components/ui/button";
-import { DatePickerDemo } from "@/components/Calendar/Calendar";
 import {BookingWizard} from "@/components/BookingWizard/BookingWizard";
 
 export default function BookingWizardPage() {
